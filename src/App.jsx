c46eceae0f84bdb9bef12a9dac529fec50a5c293
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-08-15 17:41:12 +0300

    git

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/goit-react-hw-08-phonebook/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route
             path="contacts"
